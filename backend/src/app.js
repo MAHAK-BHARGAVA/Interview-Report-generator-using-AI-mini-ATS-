bd10568226348402ec162f73aa513ab66396dc37
MAHAK-BHARGAVA <mahakbhargava165@gmail.com>
2026-06-25 02:01:52 +0530

    fix cors error

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,9 +6,20 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // ← parses form data
 app.use(cookieParser());
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://skillscopeai.vercel.app'
+];
+
 app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
 }));
 import authRouter from './routes/authroutes.js';
 import interviewRouter from './routes/interviewroutes.js';
